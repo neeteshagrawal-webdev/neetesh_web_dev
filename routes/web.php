74285e6fc74317e14c1613a404eb71d1478f1204
Fayyum khan <fayyum7869@gmail.com>
2025-02-07 19:56:38 +0530

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
+
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VlogController;
 
+// Default login page
 Route::get('/', function () {
     return view('login');
 });
+
+// User routes
 Route::get('/home/index', [UserController::class, 'home'])->name('home');
 Route::post('/home', [UserController::class, 'login'])->name('user.login');
 Route::get('/uploads', [UserController::class, 'upload_data'])->name('uploads.show');
@@ -13,9 +17,8 @@ Route::get('/scrollMessage', [UserController::class, 'scroll_message'])->name('s
 Route::get('/users', [UserController::class, 'users_view'])->name('users.show');
 Route::get('/loginActivity', [UserController::class, 'login_activity'])->name('login.activity');
 Route::get('/report', [UserController::class, 'report_show'])->name('user.report');
-Route::post('/uploadManual',  [UserController::class, 'uploadManual'])->name('uploadManual');
 
-Route::resource('vlogs', VlogController::class);
-Route::post('/vlogs/{id}/like', [VlogController::class, 'like'])->name('vlogs.like');
-Route::post('/vlogs/{id}/dislike', [VlogController::class, 'dislike'])->name('vlogs.dislike');
+// File Upload
+Route::post('/uploadManual', [UserController::class, 'uploadManual'])->name('uploadManual');
+
 
