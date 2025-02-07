50393d61ea85f31bcb03fd89a0b2c9a0b294a2c8
neetesh <neeteshagrawal55@gmail.com>
2025-02-07 19:33:43 +0530

    master functionality

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -9,7 +9,7 @@
         @if(session('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+               <!--  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> -->
             </div>
         @endif
 
@@ -55,12 +55,13 @@
                             <td>{{$data->letter_number}}</td>
                             <td>{{$data->subject_of_letter}}</td>
                             <td class="text-center">
-                                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#editCategoryModal1__id">
+                                <button class="btn btn-sm btn-success" onclick="updateData({{$data->id}})" >
                                     <i class="fas fa-edit"></i>
                                 </button>
-                                <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteCategoryModal1__id">
+                                <button class="btn btn-sm btn-danger" onclick="deleteData({{$data->id}})">
                                     <i class="fas fa-trash"></i>
                                 </button>
+                                <a href="{{$data->upload_file}}" class="btn btn-sm btn-primary" target="_blank"><i class="fa fa-eye"></i></a>
                             </td>
                         </tr>
                         <?php $i++; ?>
@@ -78,18 +79,19 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="editCategoryModalLabel">Edit FAQs</h5>
+                            <h5 class="modal-title" id="editCategoryModalLabel">Edit Upload</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <form action="" method="POST" enctype="multipart/form-data">
+                        
                            
                         <div class="modal-body">
                             
                             <div class="form-group">
                         <label for="categoryName">Category</label>
-                        <select class="form-control" id="category" name="category" required>
+                        <input type="hidden" id="upload_id">
+                        <select class="form-control" id="model_category_name" name="category" required>
                             <option value="">Please Select</option>
                             <option value="Kavach">Kavach</option>
                             <option value="LTE">LTE</option>
@@ -99,7 +101,7 @@
                     </div>
                     <div class="form-group">
                         <label for="categoryName">Subcategory</label>
-                           <select class="form-control" id="subcategory" name="subcategory" required>
+                           <select class="form-control" id="model_subcategory_name" name="subcategory" required>
                                 <option value="">Please Select</option>
                                 <option value="Brochure">Brochure</option>
                                 <option value="Advisories">Advisories</option>
@@ -108,7 +110,7 @@
                     </div>
                     <div class="form-group">
                         <label for="categoryName">Priority</label>
-                          <select class="form-control" id="Priority" name="priority" required>
+                          <select class="form-control" id="model_Priority" name="priority" required>
                                 <option value="">Please Select</option>
                                 <option value="PRIORITY">PRIORITY</option>
                                 <option value="MOST PRIORITY">MOST PRIORITY</option>
@@ -119,30 +121,30 @@
                     </div>
                     <div class="form-group">
                         <label for="categoryName">Letter Date</label>
-                        <input type="date" class="form-control" id="letter_date" name="letter_date" placeholder="Enter Question" required>
+                        <input type="date" class="form-control" id="model_letter_date" name="letter_date" placeholder="Enter Question" required>
                     </div>
                     <div class="form-group">
                         <label for="categoryName">Letter Number</label>
-                        <input type="text" class="form-control" id="letter_number" name="letter_number" placeholder="Enter Letter Number"  required>
+                        <input type="text" class="form-control" id="model_letter_number" name="letter_number" placeholder="Enter Letter Number"  required>
                     </div>
                     <div class="form-group">
                         <label for="categoryName">Subject Of Letter</label>
-                        <input type="text" class="form-control" id="subject_of_letter" name="subject_of_letter" placeholder="Enter Subject"  required>
+                        <input type="text" class="form-control" id="model_subject_of_letter" name="subject_of_letter" placeholder="Enter Subject"  required>
                     </div>
                    
-                   <div class="form-group">
+                  <!--  <div class="form-group">
                         <label for="categoryName">Upload File</label>
                         <input type="file" class="form-control" id="file" name="file" placeholder="select file"  required>
-                    </div>
+                    </div> -->
                    
                                
                             
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-success" >Update</button>
+                            <button type="submit" onclick="model_update_data()"  class="btn btn-success" >Update</button>
                         </div>
-                    </form>
+                  
                     </div>
                 </div>
             </div>
@@ -153,17 +155,18 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="deleteCategoryModalLabel">Delete FAQs</h5>
+                            <h5 class="modal-title" >Delete</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <input type="hidden" id="delete_id">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            Are you sure you want to delete the FAQs  ?
+                            Are you sure you want to delete this record ?
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <a href="" type="button" class="btn btn-danger">Delete</a>
+                            <button type="button" onclick="deleteUploadData()" class="btn btn-danger">Delete</button>
                         </div>
                     </div>
                 </div>
@@ -247,4 +250,115 @@
 
 
 @include('includes.footer')
+
+<script>
+    function deleteUploadData(){
+        let uploadId = $("#delete_id").val();
+        //alert(uploadId);
+        $.ajax({
+            url: '/deleteUpload/' + uploadId, // Route to delete user
+            type: 'DELETE',
+            data: {
+                "_token": "{{ csrf_token() }}" // CSRF protection
+            },
+            success: function(response) {
+                alert(response.message);
+                $("#deleteCategoryModal1__id").modal("hide");
+                //$('#successMessage').text(response.message).show();
+                location.reload();
+                //$("#user-" + userId).remove(); // Remove user row from table
+            },
+            error: function(response) {
+                alert("Error deleting user");
+            }
+        });
+    }
+    function deleteData(id){
+
+
+        $("#deleteCategoryModal1__id").modal("show");
+        $("#delete_id").val(id);
+        //alert(id);
+        
+
+    }
+    function model_update_data(){
+
+        let upload_id = $("#upload_id").val();
+        let category_name = $("#model_category_name").val();
+        let subcategory_name =   $("#model_subcategory_name").val();
+        let priority  =   $("#model_Priority").val();
+        let letter_date = $("#model_letter_date").val();
+        let letter_number = $("#model_letter_number").val();
+        let subject_of_letter = $("#model_subject_of_letter").val();
+         $.ajax({
+                url: "{{ route('uploadmanualdata.update') }}",
+                type: "POST",
+                data: {
+                _token: "{{ csrf_token() }}",
+                uploadId: upload_id,
+                category: category_name,
+                subcategory:subcategory_name,
+                priority:priority,
+                letter_date:letter_date,
+                letter_number:letter_number,
+                subject_of_letter:subject_of_letter
+                },
+                success: function(response) {
+                    //alert(response);
+                    if (response.success) {
+                        alert(response.message);
+                        $("#editCategoryModal1__id").modal("hide");
+                      //  location.reload();
+                    } else {
+                        alert("Update failed.");
+                    }
+                },
+                error: function() {
+                    alert("An error occurred.");
+                }
+            });
+
+
+    }
+    function updateData(id){
+
+        //alert(id);
+        $.ajax({
+        url: '/updateManualData/' + id, // Route to delete user
+        type: 'GET',
+        data: {
+        "_token": "{{ csrf_token() }}" // CSRF protection
+        },
+        success: function(response) {
+
+        if (response.success) {
+
+            $("#upload_id").val(response.upload.id);
+            $("#model_category_name").val(response.upload.category);
+            $("#model_subcategory_name").val(response.upload.subcategory);
+            $("#model_Priority").val(response.upload.priority);
+            $("#model_letter_date").val(response.upload.letter_date);
+            $("#model_letter_number").val(response.upload.letter_number);
+            $("#model_subject_of_letter").val(response.upload.subject_of_letter);
+           // console.log("Role from database:", response.upload.category);
+          
+            
+            $("#editCategoryModal1__id").modal("show");
+        } else {
+        alert("User not found!");
+        }
+       }
+        });
+
+
+    }
+
+
+      $('#editform').on('click', function(e) {
+
+        var userId = $(this).data('id');
+          alert(userId);
+      });
+</script>
 
